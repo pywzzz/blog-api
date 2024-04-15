c8f425e6ed07118fd5b34feccd8592ff38376105
pywzzz <1021640832@qq.com>
2024-04-15 17:49:48 +0800

    p32： 1.弄了user的实体和mapper。

---
+++ common/src/main/java/com/pywzzz/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.pywzzz.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pywzzz.domain.entity.User;
+
+
+/**
+ * 用户表(User)表数据库访问层
+ *
+ * @author makejava
+ * @since 2024-04-15 17:48:03
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
