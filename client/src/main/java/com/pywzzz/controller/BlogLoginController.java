4a238e025e637389be844a56992293efa2bce83c
pywzzz <1021640832@qq.com>
2024-04-18 16:16:13 +0800

    p38： 1.统一异常处理。

--- client/src/main/java/com/pywzzz/controller/BlogLoginController.java
+++ client/src/main/java/com/pywzzz/controller/BlogLoginController.java
@@ -2,20 +2,26 @@ package com.pywzzz.controller;
 
 import com.pywzzz.domain.ResponseResult;
 import com.pywzzz.domain.entity.User;
+import com.pywzzz.eunms.AppHttpCodeEnum;
+import com.pywzzz.exception.SystemException;
 import com.pywzzz.service.BlogLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BlogLoginController {
-    
+
     @Autowired
     private BlogLoginService blogLoginService;
 
     @PostMapping("/login")
     public ResponseResult login(@RequestBody User user) {
+        if (!StringUtils.hasText(user.getUserName())) {
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
         return blogLoginService.login(user);
     }
 }
