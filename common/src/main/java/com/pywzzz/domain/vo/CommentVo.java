171616287481b20dccd6787ee0d3383005cd3dd9
pywzzz <1021640832@qq.com>
2024-04-19 16:38:22 +0800

    p46： 1.在p45的接口上考虑了子评论。

--- common/src/main/java/com/pywzzz/domain/vo/CommentVo.java
+++ common/src/main/java/com/pywzzz/domain/vo/CommentVo.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -29,4 +30,6 @@ public class CommentVo {
     private Date createTime;
 
     private String username;
+
+    private List<CommentVo> children;
 }
