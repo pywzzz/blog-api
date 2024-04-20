6acce2de24e783cd0ea6dea767372b0df63fbf8e
pywzzz <1021640832@qq.com>
2024-04-20 18:43:14 +0800

    p70&71： 1.弄了用于注册的接口。

--- common/src/main/java/com/pywzzz/eunms/AppHttpCodeEnum.java
+++ common/src/main/java/com/pywzzz/eunms/AppHttpCodeEnum.java
@@ -12,6 +12,11 @@ public enum AppHttpCodeEnum {
     REQUIRE_USERNAME(504, "必需填写用户名"),
     CONTENT_NOT_NULL(506, "内容不能为空"),
     FILE_TYPE_ERROR(507, "文件类型错误，只能上传png格式的图片"),
+    USERNAME_NOT_NULL(508, "用户名不能为空"),
+    NICKNAME_NOT_NULL(509, "昵称不能为空"),
+    PASSWORD_NOT_NULL(510, "密码不能为空"),
+    EMAIL_NOT_NULL(511, "邮箱不能为空"),
+    NICKNAME_EXIST(512, "昵称已存在"),
     LOGIN_ERROR(505, "用户名或密码错误");
     int code;
     String msg;
--- common/src/main/java/com/pywzzz/service/impl/UserServiceImpl.java
+++ common/src/main/java/com/pywzzz/service/impl/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package com.pywzzz.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.pywzzz.domain.ResponseResult;
 import com.pywzzz.domain.entity.User;
 import com.pywzzz.domain.vo.UserInfoVo;
+import com.pywzzz.eunms.AppHttpCodeEnum;
+import com.pywzzz.exception.SystemException;
 import com.pywzzz.mapper.UserMapper;
 import com.pywzzz.service.UserService;
 import com.pywzzz.utils.BeanCopyUtils;
 import com.pywzzz.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 /**
  * 用户表(User)表服务实现类
@@ -19,6 +25,21 @@ import org.springframework.stereotype.Service;
 @Service("userService")
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private boolean userNameExist(String userName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUserName, userName);
+        return count(queryWrapper) > 0;
+    }
+
+    private boolean nickNameExist(String nickName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getNickName, nickName);
+        return count(queryWrapper) > 0;
+    }
+
     @Override
     public ResponseResult userInfo() {
         // 获取当前用户id
@@ -27,13 +48,45 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         User user = getById(userId);
         // 封装成UserInfoVo
         UserInfoVo vo = BeanCopyUtils.copyBean(user, UserInfoVo.class);
-        
+
         return ResponseResult.okResult(vo);
     }
 
     @Override
     public ResponseResult updateUserInfo(User user) {
         updateById(user);
+        return ResponseResult.okResult();
+    }
+
+    @Override
+    public ResponseResult register(User user) {
+        // 对数据进行非空判断
+        if (!StringUtils.hasText(user.getUserName())) {
+            throw new SystemException(AppHttpCodeEnum.USERNAME_NOT_NULL);
+        }
+        if (!StringUtils.hasText(user.getPassword())) {
+            throw new SystemException(AppHttpCodeEnum.PASSWORD_NOT_NULL);
+        }
+        if (!StringUtils.hasText(user.getEmail())) {
+            throw new SystemException(AppHttpCodeEnum.EMAIL_NOT_NULL);
+        }
+        if (!StringUtils.hasText(user.getNickName())) {
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_NOT_NULL);
+        }
+        // 对数据进行是否存在的判断
+        if (userNameExist(user.getUserName())) {
+            throw new SystemException(AppHttpCodeEnum.USERNAME_EXIST);
+        }
+        if (nickNameExist(user.getNickName())) {
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_EXIST);
+        }
+        
+        // 对密码进行加密
+        String encodePassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodePassword);
+        // 存入数据库
+        save(user);
+
         return ResponseResult.okResult();
     }
 }
