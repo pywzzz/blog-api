2d7f0d3cf8ce94a8ee8ff567d575d44467406cee
pywzzz <1021640832@qq.com>
2024-04-21 14:30:03 +0800

    p87： 1.controller的swagger配置。

--- client/src/main/java/com/pywzzz/controller/CommentController.java
+++ client/src/main/java/com/pywzzz/controller/CommentController.java
@@ -4,11 +4,13 @@ import com.pywzzz.constants.SystemConstants;
 import com.pywzzz.domain.ResponseResult;
 import com.pywzzz.domain.entity.Comment;
 import com.pywzzz.service.CommentService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/comment")
+@Api(tags = "评论", description = "评论相关接口")
 public class CommentController {
 
     @Autowired
