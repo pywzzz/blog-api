7dcb1f463aa2380449d14fa7ac678a5e89c235f3
pywzzz <1021640832@qq.com>
2024-04-21 11:02:34 +0800

    p81： 1.利用CommandLineRunner接口实现了初始化redis数据的功能。

---
+++ client/src/main/java/com/pywzzz/runner/ViewCountRunner.java
@@ -0,0 +1,34 @@
+package com.pywzzz.runner;
+
+import com.pywzzz.domain.entity.Article;
+import com.pywzzz.mapper.ArticleMapper;
+import com.pywzzz.utils.RedisCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class ViewCountRunner implements CommandLineRunner {
+
+    @Autowired
+    private ArticleMapper articleMapper;
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 查询文章的id和viewCount
+        List<Article> articles = articleMapper.selectList(null);
+        Map<String, Integer> viewCountMap = articles.stream()
+                .collect(Collectors.toMap(article -> article.getId().toString(), article -> {
+                    return article.getViewCount().intValue();//
+                }));
+        // 存储到redis
+        redisCache.setCacheMap("article:viewCount", viewCountMap);
+    }
+}
