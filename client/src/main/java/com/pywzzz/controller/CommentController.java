b83d2d73ab3ba10c7ec90950025d03cf7ec9e24f
pywzzz <1021640832@qq.com>
2024-04-21 14:43:32 +0800

    p89： 1.接口的参数的swagger配置。

--- client/src/main/java/com/pywzzz/controller/CommentController.java
+++ client/src/main/java/com/pywzzz/controller/CommentController.java
@@ -5,6 +5,8 @@ import com.pywzzz.domain.ResponseResult;
 import com.pywzzz.domain.entity.Comment;
 import com.pywzzz.service.CommentService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +26,10 @@ public class CommentController {
 
     @GetMapping("/linkCommentList")
     @ApiOperation(value = "友链评论列表", notes = "获取一页友链评论")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum", value = "页号"),
+            @ApiImplicitParam(name = "pageSize", value = "每页大小")
+    })
     public ResponseResult linkCommentList(Integer pageNum, Integer pageSize) {
         return commentService.commentList(SystemConstants.LINK_COMMENT, null, pageNum, pageSize);
     }
